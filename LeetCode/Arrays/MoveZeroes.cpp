bb70603e0978259ec268df0f8aa89573b0ef2e18
Aryan Kumar <aryan.kumar9791@gmail.com>
2025-07-20 13:41:12 +0530

    Solved Move Zeroes

---
+++ LeetCode/Arrays/MoveZeroes.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZero = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] != 0){
+                std::swap(nums[i], nums[lastNonZero]);
+                ++lastNonZero;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {0, 1, 0, 3, 12};
+    
+    sol.moveZeroes(nums);
+    
+    cout << "Array after moving zeroes:" << endl;
+    for(int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
